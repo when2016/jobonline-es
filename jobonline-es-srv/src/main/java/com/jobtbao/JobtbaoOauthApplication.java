c229941cf4fdad6ae7e9b2db110f24abe85c2182
wanghongen <wanghongen@e-sscard.com>
2020-07-01 20:30:16 +0800

    parent

--- jobonline-es-srv/src/main/java/com/jobtbao/JobtbaoOauthApplication.java
+++ jobonline-es-srv/src/main/java/com/jobtbao/JobtbaoOauthApplication.java
@@ -1,13 +1,7 @@
 package com.jobtbao;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class JobtbaoOauthApplication {
